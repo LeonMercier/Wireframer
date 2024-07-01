e39827b15d0f0950d591f09ef2ef0b8c55ae4c9c
lemercie <lemercie@student.hive.fi>
2024-07-01 14:59:10 +0300

    handle cli arguments

---
+++ src/fdf.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.c                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/01 13:41:08 by lemercie          #+#    #+#             */
+/*   Updated: 2024/07/01 14:00:43 by lemercie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fdf.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		ft_printf("Error: Filename missing\n");
+		return (1);
+	}
+	if (argc > 2)
+	{
+		ft_printf("Error: Too many arguments\n");
+		return (1);
+	}
+	// one line becomes one int array
+	// => 2D array, formatted ad [y][x]
+	// struct to store array, width and heigth
+}
