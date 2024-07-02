aa5a501c374d3de3ac579986be8fe303250e3f9c
lemercie <lemercie@student.hive.fi>
2024-07-02 11:47:03 +0300

    work on parsing input file

--- src/fdf.c
+++ src/fdf.c
@@ -6,15 +6,71 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 13:41:08 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/01 16:16:10 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/07/02 11:46:29 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
-int	read_file(t_map map, char *filename)
+int	getlen_strv(char **strv)
 {
+	int	len;
 
+	len = 0;
+	while (*strv)
+	{
+		strv++;
+		len++;
+	}
+	return (len);
+}
+
+int	add_row_to_map(t_map *map, int len)
+{
+	map->rows++;
+	//realloc
+}
+
+int	parse_line(t_map *map, char *line)
+{
+	int		err_atoi;
+	char	**strv;
+	int		i;
+
+	strv = ft_split(line, ' ');
+	if (!strv)
+		return (-1);
+	map->cols = getlen_strv(strv);
+	add_row_to_map(map, getlen_strv(strv));
+	i = 0;
+	while (*strv)
+	{
+		map->arr[map->rows - 1][i] = ft_atoi_safe(*strv, &err_atoi);
+		strv++;
+		i++;
+	}
+
+}
+
+int	read_file(t_map *map, char *filename)
+{
+	int		fd;
+	char	*line;
+
+	fd = open(filename, O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+		if (parse_line(map, line) < 0)
+		{
+			free(line);
+			return (-1);
+		}
+		free(line);
+	}
+	return (0);
 }
 
 int	main(int argc, char **argv)
@@ -31,7 +87,7 @@ int	main(int argc, char **argv)
 		ft_printf("Error: Too many arguments\n");
 		return (1);
 	}
-	read_file(map, argv[1]);
+	read_file(&map, argv[1]);
 	// one line becomes one int array
 	// => 2D array, formatted as [y][x]
 	// struct to store array, width and heigth
