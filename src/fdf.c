30f36c16eddd7d3f09b4b82c3e99ac026ce079cf
lemercie <lemercie@student.hive.fi>
2024-07-03 15:15:48 +0300

    creating a window and closing it ith ESC

--- src/fdf.c
+++ src/fdf.c
@@ -6,11 +6,12 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/01 13:41:08 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/03 14:34:59 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/07/03 15:14:21 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
+#include "../lib/MLX42/include/MLX42/MLX42.h"
 
 void	print_map(t_map *map)
 {
@@ -31,6 +32,33 @@ void	print_map(t_map *map)
 	}
 }
 
+void	kbd_hook(void *param)
+{
+	mlx_t	*mlx;
+
+	mlx = param;
+	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
+		mlx_close_window(mlx);
+}
+
+int	start_graphics(t_map *map)
+{
+	mlx_t	*mlx;
+
+	// value of assignement is value of left operand after assignement
+	if (!(mlx = mlx_init(800, 600, "FdF", true)))
+	{
+		ft_printf("Error: failed to init mlx\n");
+		return (-1);
+	}
+	mlx_loop_hook(mlx, kbd_hook, mlx);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+	(void) map;
+	return (0);
+}
+
+
 int	main(int argc, char **argv)
 {
 	t_map	map;
@@ -46,14 +74,9 @@ int	main(int argc, char **argv)
 		return (1);
 	}
 	map.arr = 0;
-/*
-	(void) argv;
-	(void) map;
-	int	test = 0;
-	ft_printf("%i\n", ft_atoi_safe(" 10 ", &test));
-*/
 	read_file(&map, argv[1]);
 	print_map(&map);
+	start_graphics(&map);
 	// one line becomes one int array
 	// => 2D array, formatted as [y][x]
 	// struct to store array, width and heigth
