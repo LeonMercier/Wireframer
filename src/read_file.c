f98316b25f909298381ea15d5f60e5b4c4dc7241
lemercie <lemercie@student.hive.fi>
2024-07-09 12:24:52 +0300

    reworking map reading once more

--- src/read_file.c
+++ src/read_file.c
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 14:21:35 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/08 12:51:00 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/07/09 12:24:05 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static int	getlen_strv(char **strv)
 	}
 	return (len);
 }
-
+/*
 static int	add_row_to_map(t_map *map)
 {
 	t_point	*new_arr;
@@ -40,11 +40,25 @@ static int	add_row_to_map(t_map *map)
 	map->rows++;
 	return (0);
 }
-
+*/
 t_point	*get_point(t_map *map, int x, int y)
 {
 	ft_printf("X: %i, Y: %i\n", x, y);
 	return (&map->arr[(map->cols * y) + x]);
+}
+
+static int	add_row_in_map(t_map *map)
+{
+	t_point *new_arr;
+
+	new_arr = (t_point *) malloc(sizeof(t_point *) * ((map->rows + 1) * map->cols));
+	if (map->rows > 0)
+	{
+		ft_memmove(new_arr, map->arr, sizeof(t_point *) * (map->rows * map->cols));
+	}
+	map->arr = new_arr;
+	map->rows++;
+	return (0);
 }
 
 static int	parse_line(t_map *map, char *line)
@@ -58,24 +72,15 @@ static int	parse_line(t_map *map, char *line)
 		return (-1);
 	map->cols = getlen_strv(strv); // assuming rectangular map, this would 
 								   // really only need to be set once
-	if (add_row_to_map(map) < 0)
-	{
-		//free strv
-		return (-1);
-	}
+	add_row_in_map(map);
 	i = 0;
 	while (*strv) 
 	{
 		err_atoi = 0;
-		get_point(map, map->rows - 1, i)->screen_x = 0;
-		get_point(map, map->rows - 1, i)->screen_y = 0;
-		get_point(map, map->rows - 1, i)->depth =
-			ft_atoi_safe2(*strv, &err_atoi);
-		//map->arr[(map->cols * i) + (map->rows - 1)].screen_x = 0;
+		get_point(map, 0, 0)->screen_x = 0;
+		get_point(map, 0, 0)->screen_y = 0;
+		get_point(map, 0, 0)->depth = ft_atoi_safe2(*strv, &err_atoi);
 		ft_printf("yay\n");
-//		map->arr[map->rows - 1][i].screen_x = -1;
-//		map->arr[map->rows - 1][i].screen_y = -1;
-//		map->arr[map->rows - 1][i].depth = ft_atoi_safe2(*strv, &err_atoi);
 		if (err_atoi)
 			ft_printf("atoi error: %i\n", err_atoi);
 		strv++;
