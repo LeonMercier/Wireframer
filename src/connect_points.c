541eff21342f8f7949b70786213ef83e5d132232
lemercie <lemercie@student.hive.fi>
2024-07-16 11:25:52 +0300

    split connect_points() into three functions

--- src/connect_points.c
+++ src/connect_points.c
@@ -6,17 +6,38 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/15 14:48:25 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/16 11:19:19 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/07/16 11:25:07 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/fdf.h"
 
+static void connect_horizontally(t_map *map, mlx_image_t *image, int x, int y)
+{
+	t_line	line;
+				
+	line.xa = lround(map->arr[y][x].screen_x);
+	line.ya = lround(map->arr[y][x].screen_y);
+	line.xb = lround(map->arr[y][x + 1].screen_x);
+	line.yb = lround(map->arr[y][x + 1].screen_y);
+	draw_line(image, line, 0xFF0000FF);
+}
+
+static void connect_vertically(t_map *map, mlx_image_t *image, int x, int y)
+{
+	t_line	line;
+				
+	line.xa = lround(map->arr[y][x].screen_x);
+	line.ya = lround(map->arr[y][x].screen_y);
+	line.xb = lround(map->arr[y + 1][x].screen_x);
+	line.yb = lround(map->arr[y + 1][x].screen_y);
+	draw_line(image, line, 0xFF0000FF);
+}
+
 void	connect_points(t_map *map, mlx_image_t *image)
 {
 	int	y;
 	int	x;
-	t_line	line;
 
 	y = 0;
 	while (y < map->rows)
@@ -26,19 +47,11 @@ void	connect_points(t_map *map, mlx_image_t *image)
 		{
 			if (x < map->cols - 1)
 			{
-				line.xa = lround(map->arr[y][x].screen_x);
-				line.ya = lround(map->arr[y][x].screen_y);
-				line.xb = lround(map->arr[y][x + 1].screen_x);
-				line.yb = lround(map->arr[y][x + 1].screen_y);
-				draw_line(image, line, 0xFF0000FF);
+				connect_horizontally(map, image, x, y);
 			}
 			if (y < map->rows - 1)
 			{
-				line.xa = lround(map->arr[y][x].screen_x);
-				line.ya = lround(map->arr[y][x].screen_y);
-				line.xb = lround(map->arr[y + 1][x].screen_x);
-				line.yb = lround(map->arr[y + 1][x].screen_y);
-				draw_line(image, line, 0xFF0000FF);
+				connect_vertically(map, image, x, y);
 			}
 			x++;
 		}
