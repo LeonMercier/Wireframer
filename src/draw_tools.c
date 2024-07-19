dad206817610486dd26fdc29abb827813884494f
lemercie <lemercie@student.hive.fi>
2024-07-19 16:05:46 +0300

    norm work

--- src/draw_tools.c
+++ src/draw_tools.c
@@ -6,7 +6,7 @@
 /*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/04 17:31:32 by lemercie          #+#    #+#             */
-/*   Updated: 2024/07/19 15:58:36 by lemercie         ###   ########.fr       */
+/*   Updated: 2024/07/19 16:03:08 by lemercie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,39 +31,39 @@ void	set_all_pixels(mlx_image_t *image, uint32_t color)
 }
 
 // Get the red channel.
-int get_r(int rgba)
+int	get_r(int rgba)
 {
-    return ((rgba >> 24) & 0xFF);
+	return ((rgba >> 24) & 0xFF);
 }
 
 // Get the green channel.
-int get_g(int rgba)
+int	get_g(int rgba)
 {
-    return ((rgba >> 16) & 0xFF);
+	return ((rgba >> 16) & 0xFF);
 }
 
 // Get the blue channel.
-int get_b(int rgba)
+int	get_b(int rgba)
 {
-    return ((rgba >> 8) & 0xFF);
+	return ((rgba >> 8) & 0xFF);
 }
 
 // Get the alpha channel.
-int get_a(int rgba)
+int	get_a(int rgba)
 {
-    return (rgba & 0xFF);
+	return (rgba & 0xFF);
 }
 
 uint32_t	get_gradient(int dx, int dy, t_line line, uint32_t color_a,
 		uint32_t color_b)
 {
-	double curlen_by_totlen;
-	uint32_t		ret_color;
+	double		curlen_by_totlen;
+	uint32_t	ret_color;
 
 	ret_color = 0;
 	curlen_by_totlen = (sqrt(pow((line.xb - line.xa), 2)
-				+ pow((line.yb - line.ya), 2)))
-				/ (sqrt((dy * dy) + (dx * dx)));
+			+ pow((line.yb - line.ya), 2)))
+		/ (sqrt((dy * dy) + (dx * dx)));
 	ret_color += trunc((get_r(color_a) * curlen_by_totlen)
 			+ (get_r(color_b) * (1 - curlen_by_totlen)));
 	ret_color = ret_color << 8;
@@ -84,8 +84,8 @@ void	draw_line(mlx_image_t *image, t_line line, uint32_t color_a,
 {
 	int	dx;
 	int	dy;
-	int				inc_x;
-	int				inc_y;
+	int	inc_x;
+	int	inc_y;
 	int	err;
 	int	new_err;
 
@@ -102,15 +102,15 @@ void	draw_line(mlx_image_t *image, t_line line, uint32_t color_a,
 	err = dx + dy;
 	while (line.xa != line.xb || line.ya != line.yb)
 	{
-		if (line.xa < 0 || (unsigned int) line.xa >= image->width ||
-				line.ya < 0 || (unsigned int) line.ya >= image->height)
+		if (line.xa < 0 || (unsigned int) line.xa >= image->width
+				|| line.ya < 0 || (unsigned int) line.ya >= image->height)
 		{
 			ft_printf("Error: pixel coords %i, %i out of bounds\n", line.ya,
-					line.xa);
+				line.xa);
 			break ;
 		}
 		mlx_put_pixel(image, line.xa, line.ya,
-				get_gradient(dx, dy, line, color_a, color_b));
+			get_gradient(dx, dy, line, color_a, color_b));
 		new_err = 2 * err;
 		if (new_err >= dy)
 		{
